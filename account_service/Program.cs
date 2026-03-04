a44c0c4880cd60c574b2a2a5783345b8cbc45eca
hansj4873 <hansengjuu@naver.com>
2026-03-05 00:20:05 +0900

    [FI-001] 버전 충돌 제거, 주석 추가

--- account_service/Program.cs
+++ account_service/Program.cs
@@ -1,10 +1,14 @@
+using account_service.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<HelloWorldService>();
 
 var app = builder.Build();
 
--- account_service/controllers/HelloWorldController.cs
+++ account_service/controllers/HelloWorldController.cs
@@ -2,17 +2,28 @@ using Microsoft.AspNetCore.Mvc;
 using account_service.Services;
 
 namespace account_service.Controllers;
+/*
+[ApiController] (어트리뷰트)
+-자동 모델 바인딩 강화
+-자동 400 응답 (유효성 실패 시)
+-파라미터 바인딩 추론
+-ProblemDetails 자동 생성
 
+ApiController + ControllerBase상속 == RestController
+Route == RequestMapping
+*/
 [ApiController]
 [Route("api/[controller]")]
 public class HelloWorldController : ControllerBase
 {
+    //이거 생성자 주입 readonly== final
     private readonly HelloWorldService _helloWorldService;
 
     public HelloWorldController(HelloWorldService helloWorldService)
     {
         _helloWorldService = helloWorldService;
     }
+    //IActionResult == ResponseEntity
     [HttpGet]
     public IActionResult HelloWorld()
     {
