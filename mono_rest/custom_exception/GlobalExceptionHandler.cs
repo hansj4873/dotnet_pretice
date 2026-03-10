af7e1cf5111aa4f1f5f2fbdc7fa272e857d44491
hansj4873 <hansengjuu@naver.com>
2026-03-11 07:35:26 +0900

    [FI-001] 기본 예외처리 비활성화

--- mono_rest/custom_exception/GlobalExceptionHandler.cs
+++ mono_rest/custom_exception/GlobalExceptionHandler.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace custom_exception;
 //@ExceptionHandler 같은 거
 public class GlobalExceptionHandler : IExceptionHandler
@@ -8,14 +10,18 @@ public class GlobalExceptionHandler : IExceptionHandler
         _logger = logger;
     }
     //IExceptionHandler가 인터페이스라 이거를 포함함
-    async ValueTask<bool> IExceptionHandler.TryHandleAsync(
+    public async ValueTask<bool> TryHandleAsync(
         HttpContext httpContext, //에러가 터진 요청과 응답
         Exception exception, //실제로 터진 에러
         CancellationToken cancellationToken //요청이 취소되었는지 확인하는 용도
         )
     {
-        //예외찍기
-        _logger.LogError(exception, "exception : {Message}", exception.Message);
+        /*
+        검색용 id, 로그찾기 부우우울펴어어언해서 씀
+        Activity.Current?.Id는 MSA환경에서도 안죽고 이어지는 id
+        httpContext.TraceIdentifier는 ASP.NET Core가 http요청마다 부여하는 id
+        */
+        var exId = Activity.Current?.Id ?? httpContext.TraceIdentifier;
         //Exception 확인, showStackTrace는 스택 트레이스를 보여줄건지 정사는 변수
         var (statusCode, title, customCode, showStackTrace) = exception switch
         {
@@ -46,13 +52,24 @@ public class GlobalExceptionHandler : IExceptionHandler
         {
             Status = statusCode,
             Title = title,
-            Detail = exception.Message
+            Detail = exception.Message,
+            Instance = httpContext.Request.Path.Value
         };
+        //예외찍기
         if (showStackTrace)
         {
-            
+            _logger.LogError(
+                exception, "{ExId}\n{CustomCode}\n{Path}\n{Message}", 
+                exId, customCode, httpContext.Request.Path.Value, exception.Message);
+        }
+        else
+        {
+            _logger.LogWarning(
+                ">{ExId}\n{CustomCode}\n{Path}\n{Message}", 
+                exId, customCode, httpContext.Request.Path.Value, exception.Message);
         }
         //내가 추가로 만든 CustomCode를 problemDetails에 추가
+        problemDetails.Extensions.Add("ExId", exId);
         problemDetails.Extensions.Add("CustomCode", customCode);
 
         httpContext.Response.StatusCode = problemDetails.Status.Value;
